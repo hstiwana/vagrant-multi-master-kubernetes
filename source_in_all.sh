84d34b1a30c3e163a32c04746150102ced75f94e
hstiwana <36938993+hstiwana@users.noreply.github.com>
2019-04-16 01:30:03 -0700

    Update source_in_all.sh

--- source_in_all.sh
+++ source_in_all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+chmod +x /vagrant/*.sh
 export k8s_rpm_ver="1.13.2-0" # yum list --showduplicates kubeadm --disableexcludes=kubernetes #1.13.2-0
 export K8S_VERSION='1.13.2' #will use during intial cluster setup with /etc/kubernetes/pki/kubeadm-config-`hostname -f`.yaml
 export cni_ver="0.6.0"
