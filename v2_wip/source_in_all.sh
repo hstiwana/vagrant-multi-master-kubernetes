2ff37be8ae486dca53d0b51de7d93956fb296bda
Hardeep Singh Tiwana <36938993+hstiwana@users.noreply.github.com>
2019-04-26 01:09:49 -0700

    Update source_in_all.sh

--- v2_wip/source_in_all.sh
+++ v2_wip/source_in_all.sh
@@ -142,5 +142,8 @@ done
 }
 # version check using ETCDCTL_API 3
 etcd_statusv3(){
+echo "etcd endpoint status"
 docker run -e CONTROLLER1_IP=${CONTROLLER1_IP} -e CONTROLLER2_IP=${CONTROLLER2_IP} -e CONTROLLER3_IP=${CONTROLLER3_IP}--rm -i --net host -v /etc/kubernetes:/etc/kubernetes k8s.gcr.io/etcd:3.2.24 /bin/sh -c "export ETCDCTL_API=3 && /usr/local/bin/etcdctl  --cert=/etc/kubernetes/pki/etcd/peer.crt --key=/etc/kubernetes/pki/etcd/peer.key --cacert=/etc/kubernetes/pki/etcd/ca.crt --endpoints https://${CONTROLLER1_IP}:2379,https://${CONTROLLER2_IP}:2379,https://${CONTROLLER3_IP}:2379 --write-out="table" endpoint status"
+echo "etcd endpoint health"
+docker run -e CONTROLLER1_IP=${CONTROLLER1_IP} -e CONTROLLER2_IP=${CONTROLLER2_IP} -e CONTROLLER3_IP=${CONTROLLER3_IP}--rm -i --net host -v /etc/kubernetes:/etc/kubernetes k8s.gcr.io/etcd:3.2.24 /bin/sh -c "export ETCDCTL_API=3 && /usr/local/bin/etcdctl  --cert=/etc/kubernetes/pki/etcd/peer.crt --key=/etc/kubernetes/pki/etcd/peer.key --cacert=/etc/kubernetes/pki/etcd/ca.crt --endpoints https://${CONTROLLER1_IP}:2379,https://${CONTROLLER2_IP}:2379,https://${CONTROLLER3_IP}:2379 --write-out="table" endpoint health"
 }
